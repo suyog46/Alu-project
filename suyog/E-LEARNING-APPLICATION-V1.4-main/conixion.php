6b9e4d67a0f63694e014edd0a5cede10f2ab8942
suyog46 <suyog.211446@ncit.edu.np>
2024-02-24 10:18:14 +0545

    htdocs suyog fully added to project

--- suyog/E-LEARNING-APPLICATION-V1.4-main/conixion.php
+++
@@ -1,9 +0,0 @@
-<?php 
-    try {
-        $con = new PDO("mysql:host=localhost;dbname=eclasse", "localhost", "6f^NG#7U7@(e@0Z=");
-        $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-      }
-       catch(PDOException $e) {
-        echo "Connection failed: " . $e->getMessage();
-      }
-?>
